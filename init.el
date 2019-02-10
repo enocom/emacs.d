60518de07c001b1105497cab21bbfedce22fb28e
Eno Compton <eno@stedi.com>
2019-02-10 08:47:30 -0700

    Update smex config to match recommended config

--- init.el
+++ init.el
@@ -186,6 +186,8 @@
 (setq smex-save-file (concat user-emacs-directory ".smex-items"))
 (smex-initialize)
 (global-set-key (kbd "M-x") 'smex)
+(global-set-key (kbd "M-X") 'smex-major-mode-commands)
+(global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
 
 
 ;; projectile
