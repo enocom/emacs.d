cb88ec6fb8ff834abc7230fbff3219a8c71f8141
Eno Compton <eno@stedi.com>
2019-05-10 10:34:32 -0600

    Extend marc-mode.

--- vendor/marc-mode.el
+++ vendor/marc-mode.el
@@ -5,6 +5,8 @@
   :global 't
   :keymap (let ((map (make-sparse-keymap)))
             (define-key map (kbd "C-h") 'delete-backward-char)
+            (define-key map (kbd "C-m") (kbd "C-j"))
+            (define-key map (kbd "C-j") (kbd "C-m"))
             map))
 
 (provide 'marc-mode)
