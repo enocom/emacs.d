124f9133c819b8a2ae52760a99e335b072933a8f
Eno Compton <eno@stedi.com>
2019-12-06 21:29:08 -0700

    Use setup for Melpa from Melpa.org
    See https://melpa.org/#/getting-started.

--- init.el
+++ init.el
@@ -8,11 +8,19 @@
 ;; Use package to install external packages.
 
 (require 'package)
-;; FIXME(eno): Delete the following TLS configuration once v.27.1 is released.
-;; See: https://debbugs.gnu.org/cgi/bugreport.cgi?bug=34341
-(setq gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3")
-(add-to-list 'package-archives
-             '("melpa-stable" . "http://stable.melpa.org/packages/") t)
+(let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
+                    (not (gnutls-available-p))))
+       (proto (if no-ssl "http" "https")))
+  (when no-ssl
+    (warn "\
+Your version of Emacs does not support SSL connections,
+which is unsafe because it allows man-in-the-middle attacks.
+There are two things you can do about this warning:
+1. Install an Emacs version that does support SSL and be safe.
+2. Remove this warning from your init file so you won't see it again."))
+  ;; Comment/uncomment these two lines to enable/disable MELPA and MELPA Stable as desired
+  ;;(add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t)
+  (add-to-list 'package-archives (cons "melpa-stable" (concat proto "://stable.melpa.org/packages/")) t))
 (package-initialize)
 (when (not package-archive-contents)
   (package-refresh-contents))
